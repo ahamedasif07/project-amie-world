8e85b557dd7037c3163e1905f2f16b2e648ea9f9
rxasif07 <rxasif31@gmail.com>
2024-08-16 21:09:06 +0600

    done

--- src/Components/Anime.jsx
+++ src/Components/Anime.jsx
@@ -8,18 +8,18 @@ const Anime = ({anime}) => {
     const {image,name,language}=anime;
     return (
         <div className=''>
-                        <div className="mt-4 hover:scale-105  transition-transform max-h-[500px] rounded-md shadow-md dark:bg-gray-100 hover:bg-gradient-to-r from-cyan-500 to-blue-500 dark:text-gray-800">
+                        <div className="mt-4  border-4 relative max-h- hover:scale-105  transition-transform max-h-[500px] rounded-md shadow-md dark:bg-gray-100  dark:text-gray-800">
 	<img src={image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-green-500" />
 	<div className="flex flex-col justify-between p-6 space-y-8">
 		<div className="space-y-2 flex justify-between">
 			<h2 className="text-xl font-semibold tracking-wide">{name}</h2>
             <div>
                 {
-                language.map(lan => <p className='hover:underline'>{lan} / </p>)
+                language.map((lan,idx) => <p key={idx} className='hover:underline whitespace-nowrap'>{lan}</p>)
                 }
             </div>
 		</div>
-		{/* <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"> <a href="">Watch Now</a></button> */}
+		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"> <a href="">Watch Now</a></button>
 	</div>
     </div>
 
--- src/Pages/Animes.jsx
+++ src/Pages/Animes.jsx
@@ -6,26 +6,43 @@ import { useState } from "react";
 const Animes = () => {
     const animes = useLoaderData()
     const [allAnime,setAllAnime]=useState(6)
+    const {anime}=animes;
 
     const handleShowAll =()=>{
         setAllAnime(allAnime + 6)
     }
+    console.log(anime)
+
+    const handleScarch =()=>{
+        const inputValue = document.getElementById('input')
+        const value = inputValue.value;
+        
+        inputValue.value = ''
+    }
 
     console.log(animes)
     return (
-        <div className="pt-20 px-3 max-w-screen-2xl bg-gradient-to-r from-cyan-500 to-blue-500">
+        <div className="pt-20 px-3 max-w-screen-2xl ">
             <h2 className="text-4xl font-bold mx-auto text-center mt-6 mb-3  border-b-2 border-white">Watch Your Favorite Anime</
             h2>
         
             <p className="text-center text-gray-900 pb-12 ">Experience something extraordinary and make the most of your time by watching anime. <br /> Immerse yourself in unique storytelling and stunning visuals that promise to captivate and entertain.</p>
-           <div className=" grid md:grid-cols-2 lg:grid-cols-3 justify-center py-4 px-3 sm:grid-cols-1 mx-auto rounded-sm lg:gap-14 md:gap-8 sm:4">
+
+             <div className="flex justify-center gap-2">
+             <input type="text" 
+             id="input"
+             placeholder="Scarch" className="input input-bordered w-full max-w-xs" />
+             <button onClick={ handleScarch} className="  bg-gray-100 px-4 py-1 border-2 rounded-full  ">Scarch</button>
+             </div>
+
+           <div className=" grid md:grid-cols-2 lg:grid-cols-3 justify-center  py-4 px-3 sm:grid-cols-1 mx-auto rounded-sm lg:gap-14 md:gap-8 sm:4">
            {
                animes.slice(0,allAnime).map(anime => <Anime anime={anime} key={anime.id}></Anime>) 
             }
             
            </div>
            <div className={allAnime >= animes.length ? 'hidden ':''}>
-           <div className="flex justify-center my-4">
+           <div className="flex justify-center py-4">
            <button onClick={handleShowAll} className="btn btn-success text-white ">Show More</button>
            </div>
            </div>
